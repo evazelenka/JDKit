1e5a10ecd36e44fb28879e01986277a3052f9042
zelenka <eva.zelya@gmail.com>
2025-06-26 20:58:01 +0300

    homework.Calculator done

---
+++ src/main/java/sem3/homework/Calculator.java
@@ -0,0 +1,34 @@
+package sem3.homework;
+//Написать класс Калькулятор (необобщенный), который содержит
+// обобщенные статические методы: sum(), multiply(), divide(),
+// subtract(). Параметры этих методов – два числа разного типа, над
+// которыми должна быть произведена операция. Методы должны
+// возвращать результат своей работы.
+
+public class Calculator {
+
+
+    public static <T extends Number, V extends Number> Number sum(T t, V v){
+       return t.doubleValue() + v.doubleValue();
+    }
+
+    public static <T extends Number, V extends Number> Number multiply(T t, V v){
+        return t.doubleValue() * v.doubleValue();
+    }
+
+    public static <T extends Number, V extends Number> Number divide(T t, V v){
+        return t.doubleValue()/v.doubleValue();
+    }
+
+    public static <T extends Number, V extends Number> Number subtract(T t, V v){
+        return t.doubleValue() - v.doubleValue();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Calculator.sum(5, 1.0f));
+        System.out.println(Calculator.multiply(5, 1.0f));
+        System.out.println(Calculator.divide(5, 1.0f));
+        System.out.println(Calculator.subtract(5, 1.0f));
+    }
+
+}
