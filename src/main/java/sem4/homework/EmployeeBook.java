39d86b4a428d2b61a9714d445af9bddf5973ddfe
zelenka <eva.zelya@gmail.com>
2025-07-01 17:11:09 +0300

    sem4.homework done

--- src/main/java/sem4/homework/EmployeeBook.java
+++ src/main/java/sem4/homework/EmployeeBook.java
@@ -1,6 +1,7 @@
 package sem4.homework;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -19,6 +20,20 @@ public class EmployeeBook {
     public Employee findEmployeeByTabel(int tabel){
         Set<Employee> set = employees.stream().collect(Collectors.toSet());
         return set.stream().filter(emp -> emp.getTabel() == tabel).findFirst().orElse(null);
+    }
+
+    public List<Integer> findNumberByName(String name){
+        List<Integer> employeesWithTheSameName = new ArrayList<>();
+        Set<Employee> set = new HashSet<>(employees);
+        set.stream().filter(emp -> emp.getName().equals(name)).forEach(emp -> employeesWithTheSameName.add(emp.getPhone()));
+        return employeesWithTheSameName;
+    }
+
+    public List<Employee> findEmployeeByExperience(int exp){
+        List<Employee> employeesWithTheSameExperience = new ArrayList<>();
+        Set<Employee> set = new HashSet<>(employees);
+        set.stream().filter(emp -> emp.getExperience() == exp).forEach(employeesWithTheSameExperience::add);
+        return employeesWithTheSameExperience;
     }
 
 }
--- src/main/java/sem4/homework/Main.java
+++ src/main/java/sem4/homework/Main.java
@@ -1,5 +1,8 @@
 package sem4.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Создать справочник сотрудников
  * Необходимо:
@@ -18,12 +21,18 @@ package sem4.homework;
 public class Main {
     public static void main(String[] args) {
         EmployeeBook eb = new EmployeeBook();
-        eb.addEmployee(new Employee(123, 1234566, "nadya", 2));
+        eb.addEmployee(new Employee(123, 1234566, "dima", 2));
         eb.addEmployee(new Employee(1236, 12345654, "dima", 2));
         eb.addEmployee(new Employee(1237, 123453436, "riko", 2));
         eb.addEmployee(new Employee(1238, 123423256, "lika", 2));
-        eb.addEmployee(new Employee(1239, 123343456, "vika", 2));
+        eb.addEmployee(new Employee(1239, 123343456, "vika", 4));
         eb.addEmployee(new Employee(1243, 1234325356, "nika", 3));
         System.out.println(eb.findEmployeeByTabel(123));
+
+        List<Integer> phones = eb.findNumberByName("lika");
+        phones.forEach(i -> System.out.println(i+ " "));
+
+        List<Employee> exp = eb.findEmployeeByExperience(2);
+        exp.forEach(i -> System.out.println(i));
     }
 }
